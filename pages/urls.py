8a4545217587a40d8e8b780bf29cc8f98d40d6f0
WangaiJM <jahkeyjohn@gmail.com>
2024-02-11 00:41:43 +0300

    pages structure

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about/', views.aboutView, name='about')
+    path('about/', views.aboutView, name='about'),
+    path('rooms/', views.roomsView, name='rooms'),
+    path('teams/', views.teamView, name='team'),
+    path('gallery/', views.galleryView, name='gallery'),
+    path('contacts/', views.contactsView, name='contacts'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,18 @@ def index(request):
     return render(request, 'index.html', {'is_home': True})
 
 def aboutView(request):
-    return render(request, 'about/about.html', {'is_home': False, 'title': 'About Us'})
+    return render(request, 'about/about.html', {'title': 'About Us'})
+
+def roomsView(request):
+    return render(request, 'rooms/index.html', { 'title': 'Rooms'})
+
+def teamView(request):
+    return render(request, 'team/index.html', { 'title': 'team'})
+
+def galleryView(request):
+    return render(request, 'gallery/index.html', { 'title': 'Gallery'})
+
+def contactsView(request):
+    return render(request, 'contacts/index.html', { 'title': 'Contact Us'})
+
+
